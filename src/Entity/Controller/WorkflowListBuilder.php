26d56528d6f7939937f3ffaf0b16ab935c60618d
johnv <john.voskuilen@sapito.nl>
2015-09-20 21:26:30 +0200

    Issue #2351299: D8-port: Workflow UI: move calls to hook_workflow_operations

--- src/Entity/Controller/WorkflowListBuilder.php
+++ src/Entity/Controller/WorkflowListBuilder.php
@@ -53,4 +53,62 @@ class WorkflowListBuilder extends ConfigEntityListBuilder {
     return $row + parent::buildRow($entity);
   }
 
+  /**
+   * {@inheritdoc}
+   */
+  public function getDefaultOperations(EntityInterface $entity) {
+    /* @var $entity \Drupal\workflow\Entity\Workflow */
+    $operations = parent::getDefaultOperations($entity);
+
+    $workflow = $entity;
+
+//    dpm('TODO D8-port: test function WorkflowState::' . __FUNCTION__ .': ' . 'top actions (via own routing??)');
+    // Allow modules to insert their own action links to the 'table', like cleanup module.
+    $top_actions = \Drupal::moduleHandler()->invokeAll('workflow_operations', ['top_actions', NULL]);
+    // @todo: add these top actions next to the core 'Add workflow' action.
+    $top_actions_args = array(
+      'links' => $top_actions,
+      'attributes' => array('class' => array('inline', 'action-links')),
+    );
+// $form['action-links'] = array(
+//       '#type' => 'markup',
+//       '#markup' => theme('links', $top_actions_args),
+//       '#weight' => -1,
+//     );
+
+
+//    dpm('TODO D8-port: test function WorkflowState::' . __FUNCTION__ .': ' . 'add default operations');
+    // Allow modules to insert their own workflow operations.
+//    foreach ($form['table']['#rows'] as &$row) {
+//      $url = $row[0]['data']['#url'];
+//      $workflow = $url['options']['entity'];
+    foreach ($actions = \Drupal::moduleHandler()->invokeAll('workflow_operations', ['workflow', $workflow]) as $action) {
+      $action['attributes'] = isset($action['attributes']) ? $action['attributes'] : array();
+
+      $operations['MY_NEW_OPERATION'] = array(
+        'title' => t('MY_NEW_OPERATION'),
+        'weight' => -10,
+        // @FIXME
+        'url' => $entity->urlInfo('MY_NEW_OPERATION'),
+//      'url'=> l(strtolower($action['title']), $action['href'], $action['attributes']);
+      );
+    }
+
+
+//    dpm('TODO D8-port: test function WorkflowState::' . __FUNCTION__ .': ' . 'rempve delete operation');
+    // Avoid the 'delete' operation if the Workflow is used somewhere.
+//    $status = $entity->status;
+//
+//    // @see parent::overviewTableRow() how to determine a deletable entity.
+//    if (!entity_has_status($this->entityType, $entity, ENTITY_IN_CODE) && !$entity->isDeletable())  {
+//      // Set to a state that does not allow deleting, but allows other actions.
+//      $entity->status = ENTITY_IN_CODE;
+//    }
+//
+//    // Just to be sure: reset status.
+//    $entity->status = $status;
+
+    return $operations;
+  }
+
 }
