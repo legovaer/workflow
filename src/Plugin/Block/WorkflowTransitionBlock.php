034b7d2e6fb254b41fc002acb3a0ed8c716e9fd2
johnv <john.voskuilen@sapito.nl>
2015-11-01 19:29:35 +0100

    Issue #2595247: [D8-port task] Add action buttons

--- src/Plugin/Block/WorkflowTransitionBlock.php
+++ src/Plugin/Block/WorkflowTransitionBlock.php
@@ -88,25 +88,6 @@ class WorkflowTransitionBlock extends BlockBase  {
     // Add the WorkflowTransitionForm to the page.
     $form = $this->entityFormBuilder()->getForm($transition, 'add');
 
-    // Add Submit buttons/Action buttons.
-    // Either a default 'Submit' button is added, or a button per permitted state.
-    $settings_options_type = '';
-    if ($settings_options_type == 'buttons') {
-      workflow_debug(__FILE__, __FUNCTION__, __LINE__);  // @todo D8-port: still test this snippet.
-      // How do action buttons work? See also d.o. issue #2187151.
-      // Create 'action buttons' per state option. Set $sid property on each button.
-      // 1. Admin sets ['widget']['options']['#type'] = 'buttons'.
-      // 2. This function formElelent() creates 'action buttons' per state option;
-      //    sets $sid property on each button.
-      // 3. User clicks button.
-      // 4. Callback _workflow_transition_form_validate_buttons() sets proper State.
-        // 5. Callback _workflow_transition_form_validate_buttons() sets Submit function.
-      // @todo: this does not work yet for the Add Comment form.
-
-      // Performance: inform workflow_form_alter() to do its job.
-      _workflow_use_action_buttons(TRUE);
-    }
-
     return $form;
   }
 
